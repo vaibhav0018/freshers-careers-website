33c88e7e90b4ba5117f20b37c75d7bd24a0f9fb6
vaibhav0018 <140957124+vaibhav0018@users.noreply.github.com>
2024-06-07 11:37:06 +0000

    added html and css

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-  return "llama2 cfis 16 tokens"
+  return render_template("home.html")
 
 
 if __name__ == "__main__":
